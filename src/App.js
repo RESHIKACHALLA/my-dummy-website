0c0c3847f0adc8ea6febfcd7ab8011f23899e5c4
221910313013 <70572181+221910313013@users.noreply.github.com>
2024-12-29 23:13:30 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import About from "./About"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Reshika's sample website</h1>
+        <p>
+          I'm interested to learn and gain hands-on experience by working with Bulk Magics. Thank you for the team in supporting recent grads.
+        </p>
+        <button>CLICK</button>
+        
+      </header>
+      <About />
+    </div>
+  );
+}
+
+export default App;
